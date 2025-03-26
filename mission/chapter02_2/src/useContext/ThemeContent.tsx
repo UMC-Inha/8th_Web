309e8d27f1021593da9074544dada4b51cebee22
uxxin <dbwlsdid1114@naver.com>
2025-03-26 21:42:47 +0900

    mission: chapter02_2 인증

---
+++ mission/chapter02_2/src/useContext/ThemeContent.tsx
@@ -0,0 +1,30 @@
+import clsx from "clsx";
+import { THEME, useTheme } from "./context/ThemeProvider";
+
+export default function ThemeContent(): JSX.Element {
+  const { theme } = useTheme();
+  const isLightMode = theme === THEME.LIGHT;
+  return (
+    <div
+      className={clsx(
+        "p-4 h-dvh w-full",
+        isLightMode ? "bg-white" : "bg-gray-800"
+      )}
+    >
+      <h1
+        className={clsx(
+          "text-wxl font-bold",
+          isLightMode ? "text-black" : "text-white"
+        )}
+      >
+        Theme Content
+      </h1>
+      <p className={clsx("mt-2", isLightMode ? "text-black" : "text-white")}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis quos
+        fugit ad deleniti at architecto eius nesciunt, libero, illum praesentium
+        distinctio? Laudantium officia blanditiis illo rerum. Fugiat iusto alias
+        natus?
+      </p>
+    </div>
+  );
+}
