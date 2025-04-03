a5e9488fecc829fb4052abc7995ad079c99124d0
sunny <lesunhe@gmail.com>
2025-04-03 21:32:01 +0900

    mission: 3주차 미션1 useEffect로 영화데이터불러오기

---
+++ mission/Chapter03/ReactUseEffect/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import MoviesPage from "./MoviesPage"; // MoviesPage 컴포넌트 임포트
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Welcome to the Movie App</h1>
+        <MoviesPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
