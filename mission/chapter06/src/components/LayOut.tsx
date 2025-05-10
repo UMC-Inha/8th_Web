352b2e5562b7948de2b782c64fcf6e68e30a91d5
uxxin <dbwlsdid1114@naver.com>
2025-05-11 04:39:38 +0900

    mission: chapter06 미션1 사이드바 반응형

--- mission/chapter06/src/components/LayOut.tsx
+++ mission/chapter06/src/components/LayOut.tsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
 const Layout = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1000) {
+        setSidebarOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="relative min-h-screen bg-black text-white">
